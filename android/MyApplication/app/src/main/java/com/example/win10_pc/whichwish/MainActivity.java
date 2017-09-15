80fb85d65d8250c5a588ade73337df07debcc946
seowok <seowok@naver.com>
2017-09-16 01:37:22 +0900

    updated by gewonyongil, android
    Signed-off-by: seowok <seowok@naver.com>

--- android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/MainActivity.java
+++ android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/MainActivity.java
@@ -1,13 +1,39 @@
-package com.example.win10_pc.whichwish;
+package mobile.cau.swopenhack;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    WishListAdapter wishListAdapter = new WishListAdapter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        final View header = getLayoutInflater().inflate(R.layout.wishlistview_header, null, false);
+        header.findViewById(R.id.add_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "ADD", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                //intent add Activity
+            }
+        });
+        listView.addHeaderView(header);
+        listView.setAdapter(wishListAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        if(intent != null){
+            wishListAdapter.addItem(intent.getExtras().getString("title"), intent.getExtras().getString("context"));
+        }
     }
 }
---
+++ android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/WishListAdapter.java
@@ -0,0 +1,66 @@
+package mobile.cau.swopenhack;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kyi42 on 2017-09-15.
+ */
+
+public class WishListAdapter extends BaseAdapter {
+    private ArrayList<WishListViewItem> listViewItems = new ArrayList<WishListViewItem>();
+
+    public WishListAdapter(){
+
+    }
+    @Override
+    public int getCount() {
+        return listViewItems.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return listViewItems.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final int pos = i;
+        final Context context = viewGroup.getContext();
+
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.wishlistview_item, viewGroup, false);
+        }
+
+        TextView title_tv = (TextView)view.findViewById(R.id.title_tv);
+        TextView content_tv = (TextView)view.findViewById(R.id.content_tv);
+
+        WishListViewItem wishListViewItem = listViewItems.get(i);
+        title_tv.setText(wishListViewItem.getTitle());
+        content_tv.setText(wishListViewItem.getContent());
+
+        return view;
+    }
+    public void addItem(String title, String content){
+        WishListViewItem item = new WishListViewItem();
+        item.setTitle(title);
+        item.setContent(title);
+
+        listViewItems.add(item);
+    }
+    public void delItem(int index){
+        listViewItems.remove(index);
+    }
+}
