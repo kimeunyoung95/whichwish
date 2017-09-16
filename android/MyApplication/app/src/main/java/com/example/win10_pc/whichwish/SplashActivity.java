691d31c47c578c03fdf935ad20a2985ba4ce60d5
kimeunyoung95 <2840345@naver.com>
2017-09-16 14:46:37 +0900

    splash

---
+++ android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.win10_pc.whichwish;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class SplashActivity extends Activity {
+
+    Handler handler = new Handler();
+
+    int time = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, time);
+    }
+}
