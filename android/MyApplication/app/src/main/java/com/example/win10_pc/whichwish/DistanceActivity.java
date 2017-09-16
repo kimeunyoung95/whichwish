e8b151b248022bea6b09235296b6dd9e35fe2c60
kimeunyoung95 <2840345@naver.com>
2017-09-16 15:14:37 +0900

    pushactivity

---
+++ android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/DistanceActivity.java
@@ -0,0 +1,14 @@
+package com.example.win10_pc.whichwish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DistanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distance);
+    }
+    
+}
