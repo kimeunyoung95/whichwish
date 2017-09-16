c3a0469b83077f388a44ae5a5ed55ea1b65958a7
gweonyongil <kyi4210@daum.net>
2017-09-16 13:02:35 +0900

    updated
    Signed-off-by: gweonyongil <kyi4210@daum.net>

--- android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/MainActivity.java
+++ android/MyApplication/app/src/main/java/com/example/win10_pc/whichwish/MainActivity.java
@@ -7,8 +7,17 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     WishListAdapter wishListAdapter = new WishListAdapter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +35,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         listView.addHeaderView(header);
+        loadData();
         listView.setAdapter(wishListAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveData();
+    }
+
+    public void saveData() {
+        try {
+            FileOutputStream fos = new FileOutputStream("t.tmp");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(wishListAdapter.getListViewItems());
+            oos.close();
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void loadData() {
+        try {
+            FileInputStream fis = new FileInputStream("t.tmp");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            wishListAdapter.setData((ArrayList<WishListViewItem>) ois.readObject());
+            ois.close();
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException e) {
+
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Intent intent = getIntent();
-        if(intent != null){
+        if (intent != null) {
             wishListAdapter.addItem(intent.getExtras().getString("title"), intent.getExtras().getString("context"));
         }
     }
